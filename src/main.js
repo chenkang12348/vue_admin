ccd1a83946d2c552eb1861e5236003d681d7124f
chenkang <2539089220@qq.com>
2020-12-16 22:17:22 +0800

    '构建项目，配置全局样式，路由，element-ui'

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 导入全局样式表
+import './styles/main.css'
+// import './styles/normalize.css'
+// 完整引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="login">
+    <h1>登录页面</h1>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
